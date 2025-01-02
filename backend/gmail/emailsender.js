b113c88baab4edafe385ae8aa5b247f18827b22d
Muhammad Wasi <156829042+muhammadwasikt@users.noreply.github.com>
2025-01-02 13:15:12 +0500

    Update

--- backend/gmail/emailsender.js
+++ backend/gmail/emailsender.js
@@ -1,14 +1,18 @@
 import nodemailer from 'nodemailer'
 import dotenv from 'dotenv'
 
-
 dotenv.config()
 
-
 export var transporter = nodemailer.createTransport({
-    service: 'gmail'
+    host: 'smtp.gmail.com',
+    port: process.env.EMAIL_SEND_PORT,
+    secure: true,
     auth: {
         user: process.env.SENDER_EMAIL,
         pass: process.env.SENDER_PASSWORD
     },
-})
+    tls: {
+        rejectUnauthorized: false, 
+    },
+});
+}
